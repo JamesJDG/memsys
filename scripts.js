161793840a9c2abf380f9ce1445032017e133e1f
JamesJDG <james01.jdg@gmail.com>
2020-06-26 11:20:41 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
-  document.write(data);
+  document.write(this.response);
   /*if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
       const card = document.createElement('div');
